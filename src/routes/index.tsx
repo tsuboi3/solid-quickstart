9e132cd9dc63521e3511353b2e6342cba0326133
tsuboi3 <66989243+tsuboi3@users.noreply.github.com>
2023-07-14 17:09:58 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,7 @@ export default function Home() {
   return (
     <main>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PK9DWF9');</script>
-<!-- End Google Tag Manager -->
+
       
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
@@ -24,11 +18,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         to learn how to build SolidStart apps.
       </p>
 
-  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PK9DWF9"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-  
     </main>
   );
 }
